ca3f0b70fff802b28395d6d4e493d9026ea4402a
chikaishiro <gran_turismo@126.com>
2024-04-01 16:03:05 +0800

    Update winget.ps1

--- Winget/winget.ps1
+++ Winget/winget.ps1
@@ -1,4 +1,9 @@
-Write-Host $(whoami)
-Start-Sleep 60
-winget --help
+
+if (!(Get-Module -ListAvailable -Name Microsoft.WinGet.Configuration)) {
+  Write-Host "WinGet is not installed"
+}
+else {
+  Write-Host "WinGet is already installed"
+}
+Start-Sleep 120
 winget configure -f configuration.dsc.yaml --accept-configuration-agreements
